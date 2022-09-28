c2d6144cf613a11f81c1e37ccad5550e4208b353
Diego Amorim <66035875+DiegoBAmorim@users.noreply.github.com>
2022-09-28 16:09:27 -0300

    update

--- server/src/server.ts
+++ server/src/server.ts
@@ -2,10 +2,28 @@ import express from 'express';
 
 const app = express();
 
-app.get('/ads', (req, res) => {
+app.get('/games', (req, res) => {
+  return res.json([]);
+});
+
+app.post('/ads', (req, res) => {
+  return res.status(201).json([]);
+});
+
+app.get('/games/:id/ads', (req, res) => {
+  const gameId = req.params.id;
+
   return res.json({
-    id: 1, name: 'diego'
-  })
+    id: gameId,
+  });
+});
+
+app.get('/ads/:id/discord', (req, res) => {
+  const adID = req.params.id;
+
+  return res.json({
+    id: adID,
+  });
 });
 
 app.listen(3333);
